9c13449db798e9dfa0d331d34318e5a8d9ee6d21
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-28 17:50:52 +0530

    index updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
     app.use(cors( {
         origin: [
       "http://localhost:5173",
-      "https://elite-tickets-frontend.vercel.app/api",
+      "https://elite-tickets-frontend.vercel.app",
     ],
     // credentials: true,
     }));
