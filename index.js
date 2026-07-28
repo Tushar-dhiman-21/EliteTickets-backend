d5579a5bc4420673450119f043ed85438bfd3ad8
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-28 14:34:25 +0530

    Update CORS configuration

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-const cors = require("cors");
-const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const authRoutes = require("./routes/auth.js");
-const eventRoutes = require("./routes/events.js");
-const bookingRoutes = require("./routes/bookings.js");
+    const cors = require("cors");
+    const express = require("express");
+    const dotenv = require("dotenv");
+    const mongoose = require("mongoose");
+    const authRoutes = require("./routes/auth.js");
+    const eventRoutes = require("./routes/events.js");  
+    const bookingRoutes = require("./routes/bookings.js");  
 
 
-dotenv.config();
+    dotenv.config();
 
-const app = express();
+    const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-
-
-
-app.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "EliteTickets Backend API is running",
-        version: "1.0.0"
-    });
-});
+    app.use(cors( {
+        origin: [
+      "http://localhost:5173",
+      "https://elite-tickets-frontend.vercel.app",
+    ],
+    credentials: true,
+    }));
+    app.use(express.json());
 
 
 
-//Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/events", eventRoutes);
-app.use("/api/bookings", bookingRoutes);
+    
+    //Routes
+    app.use("/api/auth",authRoutes)  ; 
+    app.use("/api/events",eventRoutes);
+    app.use("/api/bookings",bookingRoutes);
 
 
 
 
-// Connect to MongoDB
-mongoose
+    // Connect to MongoDB
+    mongoose
     .connect(process.env.MONGO_URL)
     .then(() => {
         console.log(" Connected to MongoDB");
@@ -45,8 +41,8 @@ mongoose
         console.log(" MongoDB Connection Error:", error);
     });
 
-const PORT = process.env.PORT || 6900;
+    const PORT = process.env.PORT || 6900;
 
-app.listen(PORT, () => {
+    app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+    });
