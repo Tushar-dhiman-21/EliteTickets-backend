c5d7b25d133b24aa801d051665124aa40e69c9c8
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-29 19:13:15 +0530

    add

--- server.js
+++ server.js
@@ -1,39 +1,66 @@
-const cors = require("cors");
-const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-
-const authRoutes = require("./routes/auth");
-const eventRoutes = require("./routes/events");
-const bookingRoutes = require("./routes/bookings");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
+const authRoutes = require('./routes/auth');
+const eventRoutes = require('./routes/events');
+const bookingRoutes = require('./routes/bookings');
+
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://elite-tickets-backend.vercel.app/"
-  ]
-}));
 
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URL )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB Connection Error:', err));
+
+const PORT = process.env.PORT || 6900;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+//database connection
+// let isConnected=false;
+
+// async function connectToMongoDB(){
+//   try{
+//     await mongoose.connect(process.env.MONGO_URL,{
+//       useNewUrlParser:true,
+//       useUnifiedTopology:true
+//     } );
+//     isConnected=true;
+//     console.log("MongoDB Connected")
+
+//   }catch(error){
+
+//     console.log("Error connecting to MongoDB",error)
+//   }
+// }
+
+
+
+
+
+// Middleware
+
+// app.use((req,res,next)=>{
+//   if(!isConnected){
+//     connectToMongoDB();
+//   }
+//   next();
+// })
+
+
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "EliteTickets Backend API is running",
-    version: "1.0.0",
-  });
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api/events", eventRoutes);
-app.use("/api/bookings", bookingRoutes);
-
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error(err));
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 module.exports = app;
