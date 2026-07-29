6e6e8f0badab876b8127bbe509e48786ee9c3db2
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-29 18:17:37 +0530

    add

--- server.js
+++ server.js
@@ -11,7 +11,50 @@ const bookingRoutes = require('./routes/bookings');
 
 const app = express();
 
+
+
+// Database Connection
+// mongoose.connect(process.env.MONGO_URL )
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.error('MongoDB Connection Error:', err));
+
+// const PORT = process.env.PORT || 6900;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+//database connection
+let isConnected=false;
+
+async function connectToMongoDB(){
+  try{
+    await mongoose.connect(process.env.MONGO_URL,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true
+    } );
+    isConnected=true;
+    console.log("MongoDB Connected")
+
+  }catch(error){
+
+    console.log("Error connecting to MongoDB",error)
+  }
+}
+
+
+
+
+
 // Middleware
+
+app.use((req,res,next)=>{
+  if(!isConnected){
+    connectToMongoDB();
+  }
+  next();
+})
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -20,10 +63,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/bookings', bookingRoutes);
 
-// Database Connection
-mongoose.connect(process.env.MONGO_URL )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error('MongoDB Connection Error:', err));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
