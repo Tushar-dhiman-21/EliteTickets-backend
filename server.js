c34422b138512cd298e56398f28ad126fa49c420
Tushar Dhiman <dhimantushar2007@gmail.com>
2026-07-29 18:43:33 +0530

    Fix axios baseURL

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://elite-tickets-frontend.vercel.app"
+    "https://elite-tickets-backend.vercel.app/"
   ]
 }));
 
